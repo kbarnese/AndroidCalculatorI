2c56a97950fded3389bc103585906e4c7b19477d
Kevin Barnese <KBarnese@chem.ucla.edu>
2021-05-25 06:53:15 -0700

    5/25 12:36AM

--- app/src/main/java/com/barnese/calculator/MainActivity.java
+++ app/src/main/java/com/barnese/calculator/MainActivity.java
@@ -5,22 +5,26 @@ import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
 import org.glassfish.jersey.client.ClientConfig;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView textAns;
     Map<Button,String> buttons = new HashMap<>();
+
+
 
 
     @Override
@@ -32,23 +36,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textAns.setText("0");
 
 
-        buttons.put(findViewById(R.id.button0), "0");
-        buttons.put(findViewById(R.id.button1), "1");
-        buttons.put(findViewById(R.id.button2), "2");
-        buttons.put(findViewById(R.id.button3), "3");
-        buttons.put(findViewById(R.id.button4), "4");
-        buttons.put(findViewById(R.id.button5), "5");
-        buttons.put(findViewById(R.id.button6), "6");
-        buttons.put(findViewById(R.id.button7), "7");
-        buttons.put(findViewById(R.id.button8), "8");
-        buttons.put(findViewById(R.id.button9), "9");
-        buttons.put(findViewById(R.id.buttondiv), "/");
-        buttons.put(findViewById(R.id.buttonplus), "+");
-        buttons.put(findViewById(R.id.buttonminus), "-");
-        buttons.put(findViewById(R.id.buttonmul), "x");
-        buttons.put(findViewById(R.id.buttonAC), "c");
-        buttons.put(findViewById(R.id.buttondot), ".");
-        buttons.put(findViewById(R.id.buttoneq), "=");
+        buttons.put((Button) findViewById(R.id.button0), "0");
+        buttons.put((Button) findViewById(R.id.button1), "1");
+        buttons.put((Button) findViewById(R.id.button2), "2");
+        buttons.put((Button) findViewById(R.id.button3), "3");
+        buttons.put((Button) findViewById(R.id.button4), "4");
+        buttons.put((Button) findViewById(R.id.button5), "5");
+        buttons.put((Button) findViewById(R.id.button6), "6");
+        buttons.put((Button) findViewById(R.id.button7), "7");
+        buttons.put((Button) findViewById(R.id.button8), "8");
+        buttons.put((Button) findViewById(R.id.button9), "9");
+        buttons.put((Button) findViewById(R.id.buttondiv), "/");
+        buttons.put((Button) findViewById(R.id.buttonplus), "+");
+        buttons.put((Button) findViewById(R.id.buttonminus), "-");
+        buttons.put((Button) findViewById(R.id.buttonmul), "x");
+        buttons.put((Button) findViewById(R.id.buttonAC), "c");
+        buttons.put((Button) findViewById(R.id.buttondot), ".");
+        buttons.put((Button) findViewById(R.id.buttoneq), "=");
 
         for(Button b : buttons.keySet() )
             b.setOnClickListener(this);
@@ -59,35 +63,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-
-
-    @Override
-    public void onClick(View v) {
+    private String client(String s){
         ClientConfig config = new ClientConfig();
         Client client = ClientBuilder.newClient(config);
         WebTarget target = client.target(getBaseURI());
 
-        String c = buttons.get(findViewById(v.getId()));
+
         Response val = null;
         try {
-             val = target.path("rest").path("hello/{info}").resolveTemplate("info", c).
-                    request(MediaType.TEXT_PLAIN).get();
+            System.out.println("TEST");
+
+            System.out.println(s);
+            val = target.path("rest").path("hello/{info}").resolveTemplate("info", s).request(MediaType.TEXT_PLAIN_TYPE).get();
+
+
+            System.out.println(val);
         }
         catch(Exception ex){
-           System.out.println(ex.getMessage());
-           ex.printStackTrace();
+            System.out.println("ERROR!!!");
+            System.out.println(ex.getMessage());
+            System.out.println(ex.getCause().toString());
+            ex.printStackTrace();
         }
-
-            String s = val.readEntity(String.class);
-            textAns.setText(s);
-
+        return val.readEntity(String.class);
+    }
 
 
+
+    @Override
+    public void onClick(View v) {
+
+        CompletableFuture<String> cf = new CompletableFuture<>();
+
+        cf.supplyAsync(() -> {return buttons.get(findViewById(v.getId()));}).
+                thenApply(t -> client(t)).thenAccept(s -> textAns.setText(s));
         }
 
     private static URI getBaseURI() {
         //here server is running on 4444 port number and project name is restfuljersey
-        return UriBuilder.fromUri("https://localhost:8080/test").build();
+        return UriBuilder.fromUri("http://10.0.2.2:8080/CalculatorBackEndII/").build();
     }
 
     }
